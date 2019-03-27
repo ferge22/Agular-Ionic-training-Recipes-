c8a011e4af941ec839a95676ddea88c7f636ae0d
ferge22 <rytisspitrys@gmail.com>
2019-03-27 18:27:55 +0200

    exctacting and dispaying route params data

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RecipesService } from '../recipes.service';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.page.scss'],
 })
 export class RecipeDetailPage implements OnInit {
+  loadedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private actvatedRoute: ActivatedRoute, private recipesSrv: RecipesService) { }
 
   ngOnInit() {
+    // Observable thats why subscribe
+    this.actvatedRoute.paramMap.subscribe(
+      paramMap => {
+        if (!paramMap.has('recipeId')) {
+          // recirect
+          return;
+        } else {
+          const recipeId = paramMap.get('recipeId');
+          this.loadedRecipe = this.recipesSrv.getRecipe(recipeId);
+        }
+      }
+    );
   }
 
 }
